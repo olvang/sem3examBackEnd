1026ee5b78f8a68eeac305a6374328b370648172
Oliver Vang <olv.vang@gmail.com>
2021-01-17 22:18:12 +0100

    DELETE FROM Sport commeted out, fix for travis

--- src/test/java/rest/SportTeamResourceTest.java
+++ src/test/java/rest/SportTeamResourceTest.java
@@ -115,7 +115,7 @@ public class SportTeamResourceTest {
             em.getTransaction().begin();
             em.createQuery("DELETE FROM User").executeUpdate();
             em.createQuery("DELETE FROM SportTeam").executeUpdate();
-            em.createQuery("DELETE FROM Sport").executeUpdate();
+            //em.createQuery("DELETE FROM Sport").executeUpdate();
             em.getTransaction().commit();
         } finally {
             em.close();
